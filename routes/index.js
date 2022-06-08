49a8884d0d3001fcd40095051fc2a4a9caeb8038
Asiflaa <asif_elias@yahoo.com>
2022-06-09 05:02:19 +0800

    add update and delete method

--- routes/index.js
+++ routes/index.js
@@ -23,5 +23,29 @@ router.post('/',async (req, res) => {
         res.redirect("/");
     }
     });
+
+// UPDATE METHOD
+router.route("/edit/:id").get((req, res) => {
+    const id = req.params.id;
+    todo.find({}, (err, tasks) => {
+        res.render("todoEdit", { todos: tasks, idTask: id });
+    });
+})
+router.post((req, res) => {
+    const id = req.params.id;
+    todo.findByIdAndUpdate(id, { content: req.body.todo }, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+});
+
+// DELETE METHOD
+router.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    todo.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
  
 module.exports = router
