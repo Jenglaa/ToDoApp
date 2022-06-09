c7aff92d56a46e92f8daa085bda63380d896e339
Asiflaa <asif_elias@yahoo.com>
2022-06-09 19:54:06 +0800

    add procfile and change app listen

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.DB_CONNECT, () => {
     console.log("Connected to db!");
 
-    app.listen(3000, () => console.log("Server Up and running"));
+    app.listen(PORT, () => console.log("Server Up and running"));
 
 });
 
